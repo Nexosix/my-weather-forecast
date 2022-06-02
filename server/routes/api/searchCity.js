62ce9060c026a6a5a5f1fc0f1e2ee39437e77d6e
Paweł Drozgowski <pawel_drozgowski@zut.edu.pl>
2022-06-02 19:28:56 +0200

    - Connected frontend with backend search engine - Added debounce to search input

--- server/routes/api/searchCity.js
+++ server/routes/api/searchCity.js
@@ -7,7 +7,9 @@ module.exports = async (req, res) => {
         return;
     }
 
-    const query = req.params.query.replace(" ", ",");
+    const query = encodeURIComponent(
+        req.params.query.replace(", ", " ").replace(" ", ",")
+    );
     const limit = 5;
 
     try {
@@ -15,9 +17,25 @@ module.exports = async (req, res) => {
             throw new Error("No API KEY found in .env file");
         }
 
-        const { data } = await axios.get(
+        let { data } = await axios.get(
             `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${process.env.API_KEY}`
         );
+
+        data.forEach((location, idx, dataCopy) => {
+            //check if any other location is near enough to be skipped
+            //and keep earlier location as it better fits query
+            dataCopy.forEach((other, otherIdx) => {
+                if (otherIdx === idx) return;
+
+                if (
+                    Math.abs(location.lat - other.lat) < 0.5 &&
+                    Math.abs(location.lon - other.lon) < 0.5
+                ) {
+                    data.splice(otherIdx, 1);
+                    dataCopy.splice(otherIdx, 1);
+                }
+            });
+        });
 
         res.json(data);
     } catch (e) {
